24eaf42ab97ed8185dec3eb9bdfd30306734f963
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-25 10:08:03 +0000

    New now creates all of the headers instead of just storing the auth header + added get helper

--- tmdb.go
+++ tmdb.go
@@ -1,13 +1,38 @@
 package tmdb
 
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+var (
+	contentType   = []string{"application/json;charset=utf-8"}
+	defaultURL, _ = url.Parse("https://api.themoviedb.org/")
+)
+
 // TMDB holds an APIv4 key for themoviedb.org
 type TMDB struct {
-	header string
+	headers http.Header
 }
 
 // New takes a TMDB APIv4 key
 func New(v4key string) *TMDB {
 	return &TMDB{
-		header: "Bearer " + v4key,
+		headers: http.Header{
+			"Authorization": []string{"Bearer " + v4key},
+			"Content-Type":  contentType,
+		},
 	}
+}
+
+func (t *TMDB) get(path string, query url.Values) (*http.Response, error) {
+	url := defaultURL
+	url.Path = path
+	url.RawQuery = query.Encode()
+	fmt.Println(url)
+	return http.DefaultClient.Do(&http.Request{
+		URL:    url,
+		Header: t.headers,
+	})
 }
