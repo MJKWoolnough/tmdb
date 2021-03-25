beb221e77f9e8ce2630e007c15b920f2591b4d37
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-25 10:42:36 +0000

    added movie search options

--- options.go
+++ options.go
@@ -36,3 +36,24 @@ type FirstAirDateYear int64
 func (f FirstAirDateYear) setParam(v url.Values) {
 	v.Set("first_air_date_year", strconv.FormatInt(int64(f), 10))
 }
+
+// Region specified a ISO 3166-1 code to filter release dates. Must be uppercase
+type Region string
+
+func (r Region) setParam(v url.Values) {
+	v.Set("region", string(r))
+}
+
+// Year filters the results by the specified year
+type Year int64
+
+func (y Year) setParam(v url.Values) {
+	v.Set("year", strconv.FormatInt(int64(y)))
+}
+
+// PrimaryReleaseYear filters the results by the specified year
+type PrimaryReleaseYear int64
+
+func (p PrimaryReleaseYear) setParam(v url.Values) {
+	v.Set("primary_release_year", strconv.FormatInt(int64(p)))
+}
