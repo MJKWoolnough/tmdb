7f54aafcc1ee87e5688e2b5a385c96e27a73a6ba
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-27 16:13:44 +0000

    added CollectionImages method

--- collections.go
+++ collections.go
@@ -36,3 +36,30 @@ func (t *TMDB) CollectionDetails(id int64, params ...option) (*CollectionDetails
 	}
 	return c, nil
 }
+
+// CollectionImage stores the data of a single image
+type CollectionImage struct {
+	AspectRatio float64 `json:"aspect_ratio"`
+	FilePath    string  `json:"file_path"`
+	Height      uint64  `json:"height"`
+	Language    *string `json:"iso_639_1"`
+	VoteAverage float64 `json:"vote_average"`
+	VoteCount   int64   `json:"vote_count"`
+	Width       int64   `json:"width"`
+}
+
+// CollectionImages stores the images details for a collection
+type CollectionImages struct {
+	ID        int64           `json:"id"`
+	Backdrops CollectionImage `json:"backdrops"`
+	Posters   CollectionImage `json:"posters"`
+}
+
+// CollectionImages retrieves collection images details by id
+func (t *TMDB) CollectionImages(id int64, params ...option) (*CollectionImages, error) {
+	c := new(CollectionImages)
+	if err := t.get(c, fmt.Sprintf("/3/collection/%d/images", id), url.Values{}, params...); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
