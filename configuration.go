339faa61296f9d8171a9b78029b786fed49a4b17
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-27 16:29:08 +0000

    added Countries method

--- configuration.go
+++ configuration.go
@@ -17,9 +17,24 @@ type Configuration struct {
 }
 
 // Configuration retrieves the system-wide configuration information
-func (t *TMDB) Configuration() (*configuration, error) {
+func (t *TMDB) Configuration() (*Configuration, error) {
 	c := new(Configuration)
-	if err := t.get(c, "/3/confiruation", url.Values{}); err != nil {
+	if err := t.get(c, "/3/configuration", url.Values{}); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+// Countries represents the list of ISO3166-1 country tags
+type Countries []struct {
+	Country     string `json:"iso_3166_1"`
+	EnglishName string `json:"english_name"`
+}
+
+// Countries retrieves the list of ISO3166-1 country tags
+func (t *TMDB) Countries() (*Countries, error) {
+	c := new(Countries)
+	if err := t.get(c, "/3/countries", url.Values{}); err != nil {
 		return nil, err
 	}
 	return c, nil
