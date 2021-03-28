ce041080a4ac41a2f831a814497318c48a109a23
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-28 14:00:15 +0100

    added tv-discover options

--- options.go
+++ options.go
@@ -78,3 +78,150 @@ type EndDate string
 func (e EndDate) setParam(v url.Values) {
 	v.Set("end_date", string(e))
 }
+
+// SortBy sorts an applicable list bythe specified key and direction
+type SortBy string
+
+func (s SortBy) setParam(v url.Values) {
+	v.Set("sort_by", string(s))
+}
+
+// AirDateGTE filters and only include TV shows that have an airy date greater than (or equal to) that specified
+type AirDateGTE string
+
+func (a AirDateGTE) setParam(v url.Values) {
+	v.Set("air_date.gte", string(a))
+}
+
+// AirDateLTE filters and only include TV shows that have any air date less than (or equal to) that specified
+type AirDateLTE string
+
+func (a AirDateLTE) setParam(v url.Values) {
+	v.Set("air_date.gte", string(a))
+}
+
+// FirstAirDateGTE filters and only include TV shows that have a first air date greater than (or equal to) that specified
+type FirstAirDateGTE string
+
+func (f FirstAirDateGTE) setParam(v url.Values) {
+	v.Set("air_date.lte", string(f))
+}
+
+// FirstAirDateLTE filters and only include TV shows that have a first air date less than (or equal to) that specified
+type FirstAirDateLTE string
+
+func (f FirstAirDateLTE) setParam(v url.Values) {
+	v.Set("first_air_date.lte", string(f))
+}
+
+// Timezone is used to set the correct timezone for date limiting filters
+type Timezone string
+
+func (t Timezone) setParam(v url.Values) {
+	v.Set("timezone", string(t))
+}
+
+// VoteAverageGTE filters by only allowing results with a vote average greater than (or equal to) that specified
+type VoteAverageGTE float64
+
+func (va VoteAverageGTE) setParam(v url.Values) {
+	v.Set("vote_average.gte", strconv.FormatFloat(float64(va), 'f', 0, 64))
+}
+
+// VoteAverageLTE filters by only allowing results with a vote average less than (or equal to) that specified
+type VoteAverageLTE float64
+
+func (va VoteAverageLTE) setParam(v url.Values) {
+	v.Set("vote_average.lte", strconv.FormatFloat(float64(va), 'f', 0, 64))
+}
+
+// WithGenres specifies a comma seperated list of genre ids to limit the results to
+type WithGenres string
+
+func (w WithGenres) setParam(v url.Values) {
+	v.Set("with_genres", string(w))
+}
+
+// WithNetworks specifies a comma seperated list of network IDs to limit the results to
+type WithNetworks string
+
+func (w WithNetworks) setParam(v url.Values) {
+	v.Set("with_networks", string(w))
+}
+
+// WithoutGenres specifies a comma seperated list of genre ids to filter from the results
+type WithoutGenres string
+
+func (w WithoutGenres) setParam(v url.Values) {
+	v.Set("without_genres", string(w))
+}
+
+// WithRuntimeGTE filters by only allowing results with a runtime greater than (or equal to) that specified
+type WithRuntimeGTE int64
+
+func (w WithRuntimeGTE) setParam(v url.Values) {
+	v.Set("with_runtime.gte", strconv.FormatFloat(float64(w), 'f', 0, 64))
+}
+
+// WithRuntimeLTE filters by only allowing results with a runtime less than (or equal to) that specified
+type WithRuntimeLTE int64
+
+func (w WithRuntimeLTE) setParam(v url.Values) {
+	v.Set("with_runtime.lte", strconv.FormatFloat(float64(w), 'f', 0, 64))
+}
+
+// IncludeNullFirstAirDates specifies that results without a first air date should be included when filtering by air date
+type IncludeNullFirstAirDates bool
+
+func (i IncludeNullFirstAirDates) setParam(v url.Values) {
+	v.Set("include_null_first_air_dates", strconv.FormatBool(bool(i)))
+}
+
+// WithOriginalLanguage specifies and ISO 639-1 string to filter results by original language
+type WithOriginalLanguage string
+
+func (w WithOriginalLanguage) setParam(v url.Values) {
+	v.Set("with_original_language", string(w))
+}
+
+// WithoutKeywords filters results by the specified keywords
+type WithoutKeywords string
+
+func (w WithoutKeywords) setParam(v url.Values) {
+	v.Set("without_keywords", string(w))
+}
+
+// ScreenedTheatrically filters results to only include those with a theatrical release
+type ScreenedTheatrically bool
+
+func (s ScreenedTheatrically) setParam(v url.Values) {
+	v.Set("screened_theatrically", strconv.FormatBool(bool(s)))
+}
+
+// WithCompanies specifies a comma seperated list of company ids to limit the results to
+type WithCompanies string
+
+func (w WithCompanies) setParam(v url.Values) {
+	v.Set("with_companies", string(w))
+}
+
+// WithKeywords filters results by the specified keywords
+type WithKeywords string
+
+func (w WithKeywords) setParam(v url.Values) {
+	v.Set("with_keywords", string(w))
+}
+
+// WithWatchProviders filters results by the specified watch proviers
+type WithWatchProviders string
+
+func (w WithWatchProviders) setParam(v url.Values) {
+	v.Set("with_watch_providers", string(w))
+}
+
+// WatchRegion filters results by the specified regions
+type WatchRegion string
+
+func (w WatchRegion) setParam(v url.Values) {
+	v.Set("watch_region", string(w))
+}
