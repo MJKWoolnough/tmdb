aa63da37cc9dfd83b4e0c8cb4c819e4541591b73
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-29 09:27:36 +0100

    split out embedded types

--- search.go
+++ search.go
@@ -4,16 +4,24 @@ import (
 	"net/url"
 )
 
-// SearchCompany is the results returned from a Company search
-type SearchCompany struct {
-	Page    uint16 `json:"page"`
-	Results []struct {
-		ID       int64   `json:"id"`
-		LogoPath *string `json:"logo_path"`
-		Name     string  `json:"name"`
-	} `json:"results"`
+// Search stores the pages fields for a returned search
+type Search struct {
+	Page         uint16 `json:"page"`
 	TotalResults uint64 `json:"total_results"`
 	TotalPages   uint64 `json:"total_pages"`
+}
+
+// CompanyResult stores a single result of a company search
+type CompanyResult struct {
+	ID       int64   `json:"id"`
+	LogoPath *string `json:"logo_path"`
+	Name     string  `json:"name"`
+}
+
+// SearchCompany is the results returned from a Company search
+type SearchCompany struct {
+	Search
+	Results []CompanyResult `json:"results"`
 }
 
 // SearchCompany searches the TMDB Movie database for the name given
@@ -25,27 +33,28 @@ func (t *TMDB) SearchCompany(query string, params ...option) (*SearchCompany, er
 	return s, nil
 }
 
+// MovieResult stores a single result of a movie search
+type MovieResult struct {
+	PosterPath       *string `json:"poster_path"`
+	Adult            bool    `json:"adult"`
+	Overview         string  `json:"overview"`
+	ReleaseDate      string  `json:"release_date"`
+	GenreIDs         []int64 `json:"genre_ids"`
+	ID               int64   `json:"id"`
+	OriginalTitle    string  `json:"original_title"`
+	OriginalLanguage string  `json:"original_language"`
+	Title            string  `json:"title"`
+	BackdropPath     *string `json:"backdrop_path"`
+	Popularity       float64 `json:"popularity"`
+	VoteCount        int64   `json:"vote_count"`
+	Video            bool    `json:"video"`
+	VoteAverage      float64 `json:"vote_average"`
+}
+
 // SearchMovie is the results returned from a Movie search
 type SearchMovie struct {
-	Page    uint16 `json:"page"`
-	Results []struct {
-		PosterPath       *string `json:"poster_path"`
-		Adult            bool    `json:"adult"`
-		Overview         string  `json:"overview"`
-		ReleaseDate      string  `json:"release_date"`
-		GenreIDs         []int64 `json:"genre_ids"`
-		ID               int64   `json:"id"`
-		OriginalTitle    string  `json:"original_title"`
-		OriginalLanguage string  `json:"original_language"`
-		Title            string  `json:"title"`
-		BackdropPath     *string `json:"backdrop_path"`
-		Popularity       float64 `json:"popularity"`
-		VoteCount        int64   `json:"vote_count"`
-		Video            bool    `json:"video"`
-		VoteAverage      float64 `json:"vote_average"`
-	} `json:"results"`
-	TotalResults uint64 `json:"total_results"`
-	TotalPages   uint64 `json:"total_pages"`
+	Search
+	Results []MovieResult `json:"results"`
 }
 
 // SearchMovie searches the TMDB Movie database for the name given
@@ -57,26 +66,27 @@ func (t *TMDB) SearchMovie(query string, params ...option) (*SearchMovie, error)
 	return s, nil
 }
 
+// TVResult stores a single result of a TV search
+type TVResult struct {
+	PosterPath       *string  `json:"poster_path"`
+	Popularity       float64  `json:"popularity"`
+	ID               int64    `json:"id"`
+	BackdropPath     string   `json:"backdrop_path"`
+	VoteAverage      float64  `json:"vote_average"`
+	Overview         string   `json:"overview"`
+	FirstAirDate     string   `json:"first_air_date"`
+	OriginCountry    []string `json:"origin_country"`
+	GenreIDs         []int64  `json:"genre_ids"`
+	OriginalLanguage string   `json:"original_language"`
+	VoteCount        int64    `json:"vote_count"`
+	Name             string   `json:"name"`
+	OriginalName     string   `json:"original_name"`
+}
+
 // SearchTV is the results returned from a TV search
 type SearchTV struct {
-	Page    uint16 `json:"page"`
-	Results []struct {
-		PosterPath       *string  `json:"poster_path"`
-		Popularity       float64  `json:"popularity"`
-		ID               int64    `json:"id"`
-		BackdropPath     string   `json:"backdrop_path"`
-		VoteAverage      float64  `json:"vote_average"`
-		Overview         string   `json:"overview"`
-		FirstAirDate     string   `json:"first_air_date"`
-		OriginCountry    []string `json:"origin_country"`
-		GenreIDs         []int64  `json:"genre_ids"`
-		OriginalLanguage string   `json:"original_language"`
-		VoteCount        int64    `json:"vote_count"`
-		Name             string   `json:"name"`
-		OriginalName     string   `json:"original_name"`
-	} `json:"results"`
-	TotalResults uint64 `json:"total_results"`
-	TotalPages   uint64 `json:"total_pages"`
+	Search
+	Results []TVResult `json:"results"`
 }
 
 // SearchTV searches the TMDB TV show database for the name given
