ee99974fe4588a29dc3dd8fd8d73d233c18464ad
Michael Woolnough <michael.woolnough@gmail.com>
2021-03-29 09:27:55 +0100

    replaced inplace type with MovieResult type

--- collections.go
+++ collections.go
@@ -7,25 +7,11 @@ import (
 
 // CollectionDetails stores a collection
 type CollectionDetails struct {
-	ID           int64  `json:"id"`
-	Name         string `json:"name"`
-	Overview     string `json:"overview"`
-	BackdropPath string `json:"backdrop_path"`
-	Parts        []struct {
-		Adult            bool    `json:"adult"`
-		GenreIDs         []int64 `json:"genre_ids"`
-		ID               int64   `json:"id"`
-		OriginalLanguage string  `json:"original_language"`
-		OriginalTitle    string  `json:"original_title"`
-		Overview         string  `json:"overview"`
-		ReleaseDate      string  `json:"release_date"`
-		PosterPath       string  `json:"poster_path"`
-		Popularity       float64 `json:"popularity"`
-		Title            string  `json:"title"`
-		Video            bool    `json:"video"`
-		VoteAverage      float64 `json:"vote_average"`
-		VoteCount        int64   `json:"vote_count"`
-	} `json:"parts"`
+	ID           int64         `json:"id"`
+	Name         string        `json:"name"`
+	Overview     string        `json:"overview"`
+	BackdropPath string        `json:"backdrop_path"`
+	Parts        []MovieResult `json:"parts"`
 }
 
 // CollectionDetails retrieves collection details by id
