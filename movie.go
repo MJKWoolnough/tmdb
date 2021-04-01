334a749b336738aec6cedeee81027222eb8974f0
Michael Woolnough <michael.woolnough@gmail.com>
2021-04-01 12:09:10 +0100

    added MovieNowPlaying method

--- movie.go
+++ movie.go
@@ -358,3 +358,22 @@ func (t *TMDB) MovieLatest(params ...option) (*MovieDetails, error) {
 	}
 	return m, nil
 }
+
+// MovieNowPlaying contains a list of currently playing movies
+type MovieNowPlaying struct {
+	Search
+	Results []MovieResult `json:"results"`
+	Dates   struct {
+		Maximum string `json:"maximum"`
+		Minimum string `json:"minimum"`
+	}
+}
+
+// MovieNowPlaying retrives a list of currently playing movies
+func (t *TMDB) MovieNowPlaying(params ...option) (*MovieNowPlaying, error) {
+	m := new(MovieNowPlaying)
+	if err := t.get(m, "/3/movie/now_playing", url.Values{}, params...); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
