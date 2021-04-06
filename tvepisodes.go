76ff9696c9cb3a1d6c9216a65d142b063da4e669
Michael Woolnough <michael.woolnough@gmail.com>
2021-04-06 17:45:35 +0100

    added TVEpisodeChanges method

--- tvepisodes.go
+++ tvepisodes.go
@@ -13,3 +13,12 @@ func (t *TMDB) TVEpisode(id int64, season int64, episode int64, params ...option
 	}
 	return tv, nil
 }
+
+// TVEpisodeChanges retrieves changes for an episode of a TV show
+func (t *TMDB) TVEpisodeChanges(id int64, season int64, episode int64, params ...option) (*EntryChanges, error) {
+	e := new(EntryChanges)
+	if err := t.get(e, fmt.Sprintf("/3/tv/%d/season/%d/episode/%d/changes", id, season, episode), url.Values{}, params...); err != nil {
+		return nil, err
+	}
+	return e, nil
+}
