e2b411a2e26cd21da2b2524a5821fbaf118b645d
Michael Woolnough <michael.woolnough@gmail.com>
2021-04-06 17:48:46 +0100

    added TVEpisodeCredits method

--- tvepisodes.go
+++ tvepisodes.go
@@ -22,3 +22,20 @@ func (t *TMDB) TVEpisodeChanges(id int64, season int64, episode int64, params ..
 	}
 	return e, nil
 }
+
+// TVEpisodeCredits contains the credit information for an episode of a TV show
+type TVEpisodeCredits struct {
+	ID         int64
+	Cast       []CastCredit `json:"cast"`
+	Crew       []CrewCredit `json:"crew"`
+	GuestStars []CastCredit `json:"guest_stars"`
+}
+
+// TVEpisodeCredits retrieves the credit information for an episode of a TV show
+func (t *TMDB) TVEpisodeCredits(id int64, season int64, episode int64, params ...option) (*TVEpisodeCredits, error) {
+	tv := new(TVEpisodeCredits)
+	if err := t.get(tv, fmt.Sprintf("/3/tv/%d/season/%d/episode/%d/credits", id, season, episode), url.Values{}, params...); err != nil {
+		return nil, err
+	}
+	return tv, nil
+}
