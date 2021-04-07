d84be8e1ec4d62d829e694c7eba689ff29e0bb98
Michael Woolnough <michael.woolnough@gmail.com>
2021-04-07 19:59:52 +0100

    broke out key version to allow for other version keys

--- tmdb.go
+++ tmdb.go
@@ -12,19 +12,37 @@ var (
 	defaultURL, _ = url.Parse("https://api.themoviedb.org/")
 )
 
-// TMDB holds an APIv4 key for themoviedb.org
-type TMDB struct {
+// APIKey is used to set the API key for a request
+type APIKey interface {
+	setAPIKey(r *http.Request, params url.Values)
+}
+
+type v4Key struct {
 	headers http.Header
 }
 
-// New takes a TMDB APIv4 key
-func New(v4key string) *TMDB {
-	return &TMDB{
+func (v v4Key) setAPIKey(r *http.Request, _ url.Values) {
+	r.Header = v.headers
+}
+
+// V4Key creates a new TMDB APIv4 key
+func V4Key(v4key string) APIKey {
+	return &v4Key{
 		headers: http.Header{
 			"Authorization": []string{"Bearer " + v4key},
 			"Content-Type":  contentType,
 		},
 	}
+}
+
+// TMDB holds an API Key to allow connection to TMDB
+type TMDB struct {
+	apiKey APIKey
+}
+
+// New takes a TMDB API key to create a TMDB connection
+func New(apiKey APIKey) *TMDB {
+	return &TMDB{apiKey}
 }
 
 // Error represents an error repsonse (401, 404) from the API
@@ -41,13 +59,13 @@ func (t *TMDB) get(result interface{}, path string, query url.Values, params ...
 	for _, param := range params {
 		param.setParam(query)
 	}
+	var req http.Request
+	t.apiKey.setAPIKey(&req, query)
 	url := defaultURL
 	url.Path = path
 	url.RawQuery = query.Encode()
-	r, err := http.DefaultClient.Do(&http.Request{
-		URL:    url,
-		Header: t.headers,
-	})
+	req.URL = url
+	r, err := http.DefaultClient.Do(&req)
 	if err != nil {
 		return err
 	}
