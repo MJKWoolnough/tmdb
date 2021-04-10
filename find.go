81bb9b76a36f26b1ebd8d3268f531606764884ca
Michael Woolnough <michael.woolnough@gmail.com>
2021-04-10 08:48:39 +0100

    added Find method

---
+++ find.go
@@ -0,0 +1,21 @@
+package tmdb
+
+import (
+	"net/url"
+)
+
+// FindResults contains the results from the Find call
+type FindResults struct {
+	MovieResults  []MovieResult  `json:"movie_results"`
+	PersonResults []PeopleResult `json:"person_results"`
+	TVResults     []TVResult     `json:"tv_result"`
+}
+
+// Find searches the database for movies, people, or TV shows that reference the given external ID
+func (t *TMDB) Find(id, source string, params ...option) (*FindResults, error) {
+	f := new(FindResults)
+	if err := t.get(f, "/3/find/"+id, url.Values{"external_source": []string{source}}, params...); err != nil {
+		return nil, err
+	}
+	return f, nil
+}
